6a5bc4236d15679a89befc04139d448924f59161
nikita-sunyata <nikitasunyata@gmail.com>
2020-12-18 23:48:26 +0800

    page 30 update

--- hello.py
+++ hello.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask import request
 from flask import current_app
 from flask import redirect
+from flask import render_template
 app = Flask(__name__) # when this program is the main program , '__main__' is going to be the parameter
 
 #two way to route
 # first is using decorator
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>'
-
+    return render_template('index.html')
+    # return '<h1>Hello World</h1>'
 #second is using app.add_url_rule() function to do so
 # def index():
 #     return '<h1>Hello World</h1>'
@@ -17,7 +18,8 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}</h1>'.format(name)
+    return render_template('user.html',name = name)
+    # return '<h1>Hello, {}</h1>'.format(name)
     # return '<h1>{}</h1>'.format(request.values)
     # return current_app.name
     # return r'<h1>{}</h1>'.format(app.url_map)
