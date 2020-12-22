6370f49a55968ef8aad0a62b24f56d420931f727
nikita-sunyata <nikitasunyata@gmail.com>
2020-12-22 09:55:56 +0800

    page 53 update

--- hello.py
+++ hello.py
@@ -7,14 +7,32 @@ from flask_bootstrap import Bootstrap
 from flask import url_for
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField , SubmitField
+from wtforms.validators import DataRequired
+from flask import session
+from flask import redirect
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
 app = Flask(__name__) # when this program is the main program , '__main__' is going to be the parameter
+app.config['SECRET_KEY'] = 'this is the secret key'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 #two way to route
 # first is using decorator
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+    # name = None
+    form = NameForm() # create an instence of NameForm() class
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',current_time=datetime.utcnow(),form=form,name=session.get('name'))
     # return '<h1>Hello World</h1>'
 #second is using app.add_url_rule() function to do so
 # def index():
@@ -47,4 +65,5 @@ def internal_server_error(e):
 if __name__ == '__main__':
     print(app.url_map)
     # print(url_for('static',filename='favicon.ico'))
+    # print(app.config)
     app.run(port=5000,debug=True)
